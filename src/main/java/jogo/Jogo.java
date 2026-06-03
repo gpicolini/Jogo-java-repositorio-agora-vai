31abe44f5f1887c5d6423deec1cdfea67196f4b0
Gabriel Picolini <apicolin03@gmail.com>
2026-06-03 20:52:34 -0300

    first commit

---
+++ src/main/java/jogo/Jogo.java
@@ -0,0 +1,43 @@
+package jogo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Jogo {
+
+    private static final String URL =
+        "jdbc:mysql://trolley.proxy.rlwy.net:52251/railway?useSSL=false&serverTimezone=UTC";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "TUEbJashZWrUFTRZyBeIYBXtUZEUILVG";
+
+    public Connection conn;
+
+    public TelaCadastro Cadastro;
+
+    public TelaLogin Login;
+
+    public Jogo() throws SQLException {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver MySQL não encontrado!");
+        }
+
+        conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        System.out.println("Conexão estabelecida com sucesso!");
+
+        Cadastro = new TelaCadastro(this);
+
+        Cadastro.Show();
+    }
+
+    public void abrirJogo(String nome) {
+
+        System.out.println("Jogador logado: " + nome);
+
+    }
+}
